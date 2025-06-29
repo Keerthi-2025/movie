0d01752a38f7655bd7cda903b692fefd6f703d7f
Keerthi <rhkeerthi3@gmail.com>
2025-06-29 15:44:32 +0530

    Added UserMapper class

---
+++ src/main/java/com/movieticket/movie/Mappers/UserMapper.java
@@ -0,0 +1,14 @@
+package com.movieticket.movie.Mappers;
+
+import com.movieticket.movie.models.User;
+
+public class UserMapper {
+    public User toUser(String userId, String username, String password, String email, String bookings){
+        return User.builder()
+                .userId(userId)
+                .username(username)
+                .password(password)
+                .bookings(bookings)
+                .build();
+    }
+}
