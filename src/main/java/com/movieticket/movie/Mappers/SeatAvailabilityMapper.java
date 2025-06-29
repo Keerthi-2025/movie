05f7b32698ae72384d98dc99b39596370b33ed85
Keerthi <rhkeerthi3@gmail.com>
2025-06-29 15:26:40 +0530

    Added SeatAvailability class

---
+++ src/main/java/com/movieticket/movie/Mappers/SeatAvailabilityMapper.java
@@ -0,0 +1,15 @@
+package com.movieticket.movie.Mappers;
+
+import com.movieticket.movie.models.Seat;
+import com.movieticket.movie.models.SeatAvailability;
+
+public class SeatAvailabilityMapper {
+    public SeatAvailability toSeatAvailbility(String seatAvailid, String seat, boolean available, String showtime){
+        return Seat.builder()
+                .seatAvailId(seatAvailid)
+                .seat(seat)
+                .available(available)
+                .showtime(showtime)
+                .build();
+    }
+}
--- src/main/java/com/movieticket/movie/models/SeatAvailability.java
+++ src/main/java/com/movieticket/movie/models/SeatAvailability.java
@@ -16,7 +16,7 @@ import java.util.List;
 @AllArgsConstructor
 public class SeatAvailability {
     @Id
-    private String id;
+    private String seatAvailid;
 
     private boolean available = true;
 
