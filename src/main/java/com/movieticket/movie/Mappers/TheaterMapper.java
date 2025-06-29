2355385bef0858eb991d3da6328006fcb80109bd
Keerthi <rhkeerthi3@gmail.com>
2025-06-29 15:40:11 +0530

    Added TheaterMapper class

--- src/main/java/com/movieticket/movie/Mappers/TheaterMapper.java
+++ src/main/java/com/movieticket/movie/Mappers/TheaterMapper.java
@@ -1,4 +1,13 @@
 package com.movieticket.movie.Mappers;
 
+import com.movieticket.movie.models.Theater;
+
 public class TheaterMapper {
+    public Theater toTheater(String theaterId, String location, String seats){
+        return Theater.builder()
+                .theaterId(theaterId)
+                .location(location)
+                .seats(seats)
+                .build();
+    }
 }
