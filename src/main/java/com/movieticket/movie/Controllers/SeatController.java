1bcdb0d756ec2baa9e57a6790bda51912f2d6672
Keerthi <rhkeerthi3@gmail.com>
2025-06-30 23:56:16 +0530

    Modified seatController

--- src/main/java/com/movieticket/movie/Controllers/SeatController.java
+++ src/main/java/com/movieticket/movie/Controllers/SeatController.java
@@ -1,6 +1,7 @@
 package com.movieticket.movie.Controllers;
 
 
+import com.movieticket.movie.Dto.Request.CreateSeatDto;
 import com.movieticket.movie.services.seat_service.SeatService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,5 +19,8 @@ public class SeatController {
     }
 
     @PostMapping("/v1/createSeat")
-    ResponseEntity<String>createSeat(@RequestBody CreateSe)
+    ResponseEntity<String>createSeat(@RequestBody CreateSeatDto data){
+        String message = seatService.createSeat(data.seatId(), data.seatRow(), data.seatNumber(), data.seatType(), data.seatSection(), data.theater());
+        return  ResponseEntity.status(201).body(message);
+    }
 }
