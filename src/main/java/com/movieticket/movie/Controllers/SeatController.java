bb126c3b7cfd5a9508dffe196f7e665373a0a172
Keerthi <rhkeerthi3@gmail.com>
2025-06-30 23:48:37 +0530

    Added content to SeatController

--- src/main/java/com/movieticket/movie/Controllers/SeatController.java
+++ src/main/java/com/movieticket/movie/Controllers/SeatController.java
@@ -1,4 +1,22 @@
 package com.movieticket.movie.Controllers;
 
+
+import com.movieticket.movie.services.seat_service.SeatService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/web/api/Seat")
 public class SeatController {
+    private SeatService seatService;
+
+    public SeatController(SeatService seatService){
+        this.seatService = seatService;
+    }
+
+    @PostMapping("/v1/createSeat")
+    ResponseEntity<String>createSeat(@RequestBody CreateSe)
 }
