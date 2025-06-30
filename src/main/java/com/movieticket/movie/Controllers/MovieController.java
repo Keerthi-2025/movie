f9aadf3ec23e9c86361299035a15e93256775270
Keerthi <rhkeerthi3@gmail.com>
2025-06-30 21:00:42 +0530

    Added MovieController class for Movie Entity

--- src/main/java/com/movieticket/movie/Controllers/MovieController.java
+++ src/main/java/com/movieticket/movie/Controllers/MovieController.java
@@ -1,9 +1,12 @@
 package com.movieticket.movie.Controllers;
 
 
+import com.movieticket.movie.Dto.Request.CreateMovieDto;
 import com.movieticket.movie.services.movie_service.MovieService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,5 +18,9 @@ public class MovieController {
         this.movieService = movieService;
     }
 
-    @PostMapping("/v1/")
+    @PostMapping("/v1/createMovie")
+    public ResponseEntity<String>createMovie(@RequestParam CreateMovieDto data){
+        String message = movieService.createMovie(data.movieId(), data.title(), data.genre(), data.duration(), data.rating(), data.poster());
+        return ResponseEntity.status(201).body(message);
+    }
 }
