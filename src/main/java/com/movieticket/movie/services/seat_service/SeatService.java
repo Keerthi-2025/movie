34e0f910c5e521d1dbe4180cc9ea517f02d70576
Keerthi <rhkeerthi3@gmail.com>
2025-06-30 23:22:32 +0530

    Added content to SeatService

--- src/main/java/com/movieticket/movie/services/seat_service/SeatService.java
+++ src/main/java/com/movieticket/movie/services/seat_service/SeatService.java
@@ -1,4 +1,11 @@
 package com.movieticket.movie.services.seat_service;
 
+
+import com.movieticket.movie.models.Theater;
+import org.springframework.stereotype.Service;
+
+@Service
 public interface SeatService {
+
+    String createSeat(String seatId, String seatRow, String seatNumber,String seatType, String seatSection, Theater theater);
 }
