c0b9ec5636b0c1f3e700b55dc8e6d0db070fbc51
Keerthi <rhkeerthi3@gmail.com>
2025-06-30 20:38:47 +0530

    Added MovieMapper

--- src/main/java/com/movieticket/movie/mappers/SeatAvailabilityMapper.java
+++ src/main/java/com/movieticket/movie/mappers/SeatAvailabilityMapper.java
@@ -1,12 +1,21 @@
 package com.movieticket.movie.mappers;
 
-import com.movieticket.movie.models.Seat;
-import com.movieticket.movie.models.SeatAvailability;
-import com.movieticket.movie.models.Showtime;
+import com.movieticket.movie.models.*;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SeatAvailabilityMapper {
+
+    public Movie toMovie(String movieId, String title, Genre genre,String duration, String rating, String poster){
+        return Movie.builder()
+                .movieId(movieId)
+                .title(title)
+                .genre(genre)
+                .duration(duration)
+                .rating(rating)
+                .poster(poster)
+                .build();
+    }
 
 
 }
