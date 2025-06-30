9c429dadef7b9d07ea04a04a8234f4be8957c902
Keerthi <rhkeerthi3@gmail.com>
2025-06-30 23:43:51 +0530

    Modified SeatServiceImpl

--- src/main/java/com/movieticket/movie/services/seat_service/SeatServiceImpl.java
+++ src/main/java/com/movieticket/movie/services/seat_service/SeatServiceImpl.java
@@ -1,7 +1,26 @@
 package com.movieticket.movie.services.seat_service;
 
+import com.movieticket.movie.mappers.SeatMapper;
 import com.movieticket.movie.models.Seat;
+import com.movieticket.movie.models.Theater;
+import com.movieticket.movie.repositories.SeatRepository;
+import com.movieticket.movie.utils.UUIDUtil;
 
 public class SeatServiceImpl implements SeatService {
-    pri
+    private SeatRepository seatRepository;
+    private SeatMapper seatMapper;
+    private UUIDUtil uuidUtil;
+
+    public SeatServiceImpl(SeatRepository seatRepository, SeatMapper seatMapper, UUIDUtil uuidUtil){
+        this.seatRepository = seatRepository;
+        this.seatMapper = seatMapper;
+        this.uuidUtil = uuidUtil;
+    }
+
+    @Override
+    public String createSeat(String seatId, String seatRow, String seatNumber, String seatType, String seatSection, Theater theater) {
+        Seat seat = seatMapper.toSeat(uuidUtil.generateUuid(), seatRow,seatNumber,seatType,seatSection,theater);
+        seatRepository.save(seat);
+        return "Seat Created";
+    }
 }
