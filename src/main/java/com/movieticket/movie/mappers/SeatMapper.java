27ade86e4d25bafe3681bcc3a84f581d8c88df1c
Keerthi <rhkeerthi3@gmail.com>
2025-06-30 23:17:30 +0530

    Added SeatMapper

--- src/main/java/com/movieticket/movie/mappers/SeatMapper.java
+++ src/main/java/com/movieticket/movie/mappers/SeatMapper.java
@@ -1,11 +1,24 @@
 package com.movieticket.movie.mappers;
 
 import com.movieticket.movie.models.Seat;
+import com.movieticket.movie.models.SeatAvailability;
 import com.movieticket.movie.models.Theater;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SeatMapper {
+
+    public Seat toSeat(String seatId, String seatRow, String seatNumber, String seatType, String seatSection, Theater theater){
+        return Seat.builder()
+                .seatId(seatId)
+                .seatRow(seatRow)
+                .seatNumber(seatNumber)
+                .seatType(seatType)
+                .seatSection(seatSection)
+                .theater(theater)
+                //.availabilities(availabilities)
+                .build();
+    }
 
 
 }
