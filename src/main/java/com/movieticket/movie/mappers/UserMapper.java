b2e2ed70d567139fe4de4e5fd48c00db004b0499
Keerthi <rhkeerthi3@gmail.com>
2025-07-01 20:37:12 +0530

    Modified UserMapper class

--- src/main/java/com/movieticket/movie/mappers/UserMapper.java
+++ src/main/java/com/movieticket/movie/mappers/UserMapper.java
@@ -8,5 +8,14 @@ import java.util.List;
 @Service
 public class UserMapper {
 
+    public User toUser(String userId, String username, String password, String email){
+        return User.builder()
+                .userId(userId)
+                .username(username)
+                .password(password)
+                .email(email)
+                .build();
+    }
+
 
 }
