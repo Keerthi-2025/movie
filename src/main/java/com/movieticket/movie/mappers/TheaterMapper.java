1d3a10560ff905e827fd0fb215992a2019d455d9
Keerthi <rhkeerthi3@gmail.com>
2025-07-01 15:11:27 +0530

    Added content to TheaterMapper

--- src/main/java/com/movieticket/movie/mappers/TheaterMapper.java
+++ src/main/java/com/movieticket/movie/mappers/TheaterMapper.java
@@ -8,5 +8,11 @@ import java.util.List;
 @Service
 public class TheaterMapper {
 
-
+public Theater toTheater(String theaterId, String name, String location){
+    return Theater.builder()
+            .theaterId(theaterId)
+            .name(name)
+            .location(location)
+            .build();
+}
 }
