9f3b0f6d3afd53231b0f1440a7a7eb6ee95e72b8
Keerthi <rhkeerthi3@gmail.com>
2025-07-01 19:41:32 +0530

    Modified Config class and TheaterController class

--- src/main/java/com/movieticket/movie/Controllers/TheaterController.java
+++ src/main/java/com/movieticket/movie/Controllers/TheaterController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/web/Theater")
+@RequestMapping("/web/api/Theater")
 public class TheaterController {
     private  final TheaterService theaterService;
 
