e8dcb022051ed38ca798c8c0b2f6f8b042946212
Keerthi <rhkeerthi3@gmail.com>
2025-07-01 20:42:50 +0530

    Modified UserService class

--- src/main/java/com/movieticket/movie/services/user_service/UserService.java
+++ src/main/java/com/movieticket/movie/services/user_service/UserService.java
@@ -1,4 +1,17 @@
 package com.movieticket.movie.services.user_service;
 
+
+import com.movieticket.movie.models.User;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public interface UserService {
+
+    String createUser(String userId, String username, String password, String email);
+
+    List<User>getAllUsers();
+
+    User getUserId(String userId);
 }
