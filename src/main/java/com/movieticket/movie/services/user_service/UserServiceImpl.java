9d5dd25c01825bd987425e8ef0160a79288bd04c
Keerthi <rhkeerthi3@gmail.com>
2025-07-01 20:45:22 +0530

    Modified UserServiceImpl  class

--- src/main/java/com/movieticket/movie/services/user_service/UserServiceImpl.java
+++ src/main/java/com/movieticket/movie/services/user_service/UserServiceImpl.java
@@ -1,4 +1,20 @@
 package com.movieticket.movie.services.user_service;
 
-public class UserServiceImpl {
+
+import com.movieticket.movie.mappers.UserMapper;
+import com.movieticket.movie.repositories.UserRepository;
+import com.movieticket.movie.utils.UUIDUtil;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl  implements  UserService{
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+    private final UUIDUtil uuidUtil;
+
+    private UserServiceImpl(UserRepository userRepository, UserMapper userMapper, UUIDUtil uuidUtil){
+        this.userRepository = userRepository;
+        this.userMapper = userMapper;
+        this.uuidUtil = uuidUtil;
+    }
 }
