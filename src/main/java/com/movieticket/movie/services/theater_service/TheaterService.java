9a00f9c84885b64bf7f232bbcdec2591e3cf6493
Keerthi <rhkeerthi3@gmail.com>
2025-07-01 15:19:14 +0530

    Modified TheaterService Interface

--- src/main/java/com/movieticket/movie/services/theater_service/TheaterService.java
+++ src/main/java/com/movieticket/movie/services/theater_service/TheaterService.java
@@ -1,4 +1,9 @@
 package com.movieticket.movie.services.theater_service;
 
+
+import org.springframework.stereotype.Service;
+
+@Service
 public interface TheaterService {
+    String createTheater(String theaterId, String name, String location);
 }
