eb9ad62f94997e2d936b853200d88374c6c8e46c
Keerthi <rhkeerthi3@gmail.com>
2025-07-01 19:49:06 +0530

    Modified TheaterService class

--- src/main/java/com/movieticket/movie/services/theater_service/TheaterService.java
+++ src/main/java/com/movieticket/movie/services/theater_service/TheaterService.java
@@ -1,9 +1,14 @@
 package com.movieticket.movie.services.theater_service;
 
 
+import com.movieticket.movie.models.Theater;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public interface TheaterService {
     String createTheater(String theaterId, String name, String location);
+
+    List<Theater> getAllTheaters();
 }
