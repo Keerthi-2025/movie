92f01babdc1d50ef4b1b7d4a3424bd6d26939947
Keerthi <rhkeerthi3@gmail.com>
2025-07-01 20:48:22 +0530

    Modified UserServiceImpl  class

--- src/main/java/com/movieticket/movie/services/user_service/UserServiceImpl.java
+++ src/main/java/com/movieticket/movie/services/user_service/UserServiceImpl.java
@@ -1,10 +1,14 @@
 package com.movieticket.movie.services.user_service;
 
 
+import com.movieticket.movie.Exceptions.ApiRequestException;
 import com.movieticket.movie.mappers.UserMapper;
+import com.movieticket.movie.models.User;
 import com.movieticket.movie.repositories.UserRepository;
 import com.movieticket.movie.utils.UUIDUtil;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserServiceImpl  implements  UserService{
@@ -16,5 +20,22 @@ public class UserServiceImpl  implements  UserService{
         this.userRepository = userRepository;
         this.userMapper = userMapper;
         this.uuidUtil = uuidUtil;
+    }
+
+    @Override
+    public String createUser(String userId, String username, String password, String email) {
+        User user = userMapper.toUser(userId,username,password,email);
+        userRepository.save(user);
+        return "User Created";
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User getUserId(String userId) {
+        return userRepository.findById(userId).orElseThrow(()->new ApiRequestException("User ID does not exists"));
     }
 }
