a0bf56d1141d572c014f5a884e3255e686a5b928
Keerthi <rhkeerthi3@gmail.com>
2025-07-01 15:23:54 +0530

    Modified TheaterServiceImpl class

--- src/main/java/com/movieticket/movie/services/theater_service/TheaterServiceImpl.java
+++ src/main/java/com/movieticket/movie/services/theater_service/TheaterServiceImpl.java
@@ -1,4 +1,29 @@
 package com.movieticket.movie.services.theater_service;
 
-public class TheaterServiceImpl {
+
+import com.movieticket.movie.mappers.TheaterMapper;
+import com.movieticket.movie.models.Theater;
+import com.movieticket.movie.repositories.TheaterRepository;
+import com.movieticket.movie.utils.UUIDUtil;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TheaterServiceImpl implements TheaterService{
+    private final TheaterRepository theaterRepository;
+    private final TheaterMapper theaterMapper;
+    private final UUIDUtil uuidUtil;
+
+    public TheaterServiceImpl(TheaterRepository theaterRepository,TheaterMapper theaterMapper, UUIDUtil uuidUtil){
+        this.theaterRepository = theaterRepository;
+        this.theaterMapper = theaterMapper;
+        this.uuidUtil = uuidUtil;
+    }
+
+
+    @Override
+    public String createTheater(String theaterId, String name, String location) {
+        Theater theater = theaterMapper.toTheater(theaterId,name,location);
+        theaterRepository.save(theater);
+        return "Theater created";
+    }
 }
