847e81dc0a416f0655e0c9a72a8c61200ad465a6
Keerthi <rhkeerthi3@gmail.com>
2025-07-02 23:02:26 +0530

    Modified ShowtimeServiceImpl class

--- src/main/java/com/movieticket/movie/models/Booking.java
+++ src/main/java/com/movieticket/movie/models/Booking.java
@@ -40,3 +40,46 @@ public class Booking {
 
 
 }
+
+package com.movieticket.movie.models;
+
+import jakarta.persistence.*;
+        import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "Booking", uniqueConstraints = @UniqueConstraint(
+        columnNames = {"seat_availability_id", "showtimeId"}
+))
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Booking {
+    @Id
+    private String bookingId;
+    private LocalDateTime bookingDate;
+
+    @Enumerated(EnumType.STRING)
+    private PaymentMethod paymentMethod;
+
+    @Enumerated(EnumType.STRING)
+    private PaymentStatus paymentStatus;
+
+    @ManyToOne
+    @JoinColumn(name = "userId")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "seat_availability_id")
+    private SeatAvailability seatAvailability;
+
+    @ManyToOne
+    @JoinColumn(name = "showtimeId")
+    private Showtime showtime;
+
+
+}
