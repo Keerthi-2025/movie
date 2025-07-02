12ee48a4c49a01ee8db778a5e07419b31478faf4
Keerthi <rhkeerthi3@gmail.com>
2025-07-02 20:41:46 +0530

    Modified ShowtimeService class

--- src/main/java/com/movieticket/movie/services/showtime_service/ShowtimeService.java
+++ src/main/java/com/movieticket/movie/services/showtime_service/ShowtimeService.java
@@ -1,4 +1,14 @@
 package com.movieticket.movie.services.showtime_service;
 
+
+import com.movieticket.movie.models.Movie;
+import com.movieticket.movie.models.Theater;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
 public interface ShowtimeService {
+
+    String createShowtime(String showtimeId, Movie movie, Theater theater, LocalDateTime startTime,LocalDateTime endTime);
 }
