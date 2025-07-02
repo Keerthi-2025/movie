de418cf5d225364d0f081c87aa04f768c17c67e8
Keerthi <rhkeerthi3@gmail.com>
2025-07-02 20:37:04 +0530

    Added content ot ShowtimeMapper class

--- src/main/java/com/movieticket/movie/mappers/ShowtimeMapper.java
+++ src/main/java/com/movieticket/movie/mappers/ShowtimeMapper.java
@@ -1,11 +1,23 @@
 package com.movieticket.movie.mappers;
 
+import com.movieticket.movie.models.Movie;
 import com.movieticket.movie.models.Showtime;
+import com.movieticket.movie.models.Theater;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 
 @Service
 public class ShowtimeMapper {
 
-
+public Showtime toShowtime(String showtimeId, Movie movie, Theater theater, LocalDateTime startTime,LocalDateTime endTime){
+    return Showtime.builder()
+            .showtimeId(showtimeId)
+            .movie(movie)
+            .theater(theater)
+            .startTime(startTime)
+            .endTime(endTime)
+            .build();
+}
 }
