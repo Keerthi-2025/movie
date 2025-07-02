ce8b70b0b7f0750c1a2314bb9e3ee6aa4b47b109
Keerthi <rhkeerthi3@gmail.com>
2025-07-02 20:46:15 +0530

    Modified ShowtimeServiceImpl class

--- src/main/java/com/movieticket/movie/services/showtime_service/ShowtimeServiceImpl.java
+++ src/main/java/com/movieticket/movie/services/showtime_service/ShowtimeServiceImpl.java
@@ -1,4 +1,20 @@
 package com.movieticket.movie.services.showtime_service;
 
-public class ShowtimeServiceImpl {
+
+import com.movieticket.movie.mappers.ShowtimeMapper;
+import com.movieticket.movie.repositories.ShowtimeRepository;
+import com.movieticket.movie.utils.UUIDUtil;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ShowtimeServiceImpl implements ShowtimeService{
+    private final ShowtimeRepository showtimeRepository;
+    private final ShowtimeMapper showtimeMapper;
+    private final UUIDUtil uuidUtil;
+
+    public ShowtimeServiceImpl(ShowtimeRepository showtimeRepository, ShowtimeMapper showtimeMapper,UUIDUtil uuidUtil){
+        this.showtimeRepository = showtimeRepository;
+        this.showtimeMapper =  showtimeMapper;
+        this.uuidUtil = uuidUtil;
+    }
 }
