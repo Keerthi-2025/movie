58a230bded24856ed45c296b19f489b499437897
Keerthi <rhkeerthi3@gmail.com>
2025-07-03 19:51:50 +0530

    Modified CreateShowtimeDto

--- src/main/java/com/movieticket/movie/Dto/Request/CreateShowtimeDto.java
+++ src/main/java/com/movieticket/movie/Dto/Request/CreateShowtimeDto.java
@@ -1,4 +1,9 @@
 package com.movieticket.movie.Dto.Request;
 
-public record CreateShowtimeDto() {
+import com.movieticket.movie.models.Movie;
+import com.movieticket.movie.models.Theater;
+
+import java.time.LocalDateTime;
+
+public record CreateShowtimeDto(String showtimeId, Movie movie, Theater theater, LocalDateTime startTime,LocalDateTime endTime) {
 }
