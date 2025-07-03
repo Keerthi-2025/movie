015d0c9af9e1323e88f4a52f50ee1c06d8221ac4
Keerthi <rhkeerthi3@gmail.com>
2025-07-03 20:02:11 +0530

    Modified CreateShowtimeController

--- src/main/java/com/movieticket/movie/Controllers/ShowtimeController.java
+++ src/main/java/com/movieticket/movie/Controllers/ShowtimeController.java
@@ -1,12 +1,16 @@
 package com.movieticket.movie.Controllers;
 
 
+import com.movieticket.movie.Dto.Request.CreateSeatDto;
+import com.movieticket.movie.Dto.Request.CreateShowtimeDto;
+import com.movieticket.movie.models.Seat;
+import com.movieticket.movie.models.Showtime;
 import com.movieticket.movie.services.showtime_service.ShowtimeService;
 import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("web/api/Showtime")
@@ -18,6 +22,19 @@ public class ShowtimeController {
         this.showtimeService = showtimeService;
     }
 
-    @PostMapping("v1/createShowtime")
-    ResponseEntity<String>createShowtime
+    @PostMapping("/v1/createShowtime")
+    ResponseEntity<String>createShowtime(@RequestBody CreateShowtimeDto data){
+        String message = showtimeService.createShowtime(data.showtimeId(), data.movie(),data.theater(),data.startTime(),data.endTime());
+        return  ResponseEntity.status(201).body(message);
+    }
+
+    @GetMapping("/v1/getAllShowtimes")
+    ResponseEntity<List<Showtime>>getAllShowtimes(){
+        return ResponseEntity.status(200).body(showtimeService.getAllShowtimes());
+    }
+
+    @GetMapping("/v1/getShowtimeId")
+    ResponseEntity<Showtime>getShowtimeId(@RequestParam("showtimeId")String showtimeId){
+        return ResponseEntity.status(200).body(showtimeService.getShowtimeId(showtimeId));
+    }
 }
