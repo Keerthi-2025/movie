d13337eb9284148ece5ed1331c1acb3d90b15de8
Keerthi <rhkeerthi3@gmail.com>
2025-07-03 19:48:56 +0530

    Created ShowtimeDto file and moified ShowtimeController

--- src/main/java/com/movieticket/movie/Controllers/ShowtimeController.java
+++ src/main/java/com/movieticket/movie/Controllers/ShowtimeController.java
@@ -1,4 +1,23 @@
 package com.movieticket.movie.Controllers;
 
+
+import com.movieticket.movie.services.showtime_service.ShowtimeService;
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("web/api/Showtime")
 public class ShowtimeController {
+
+    private ShowtimeService showtimeService;
+
+    public ShowtimeController(ShowtimeService showtimeService){
+        this.showtimeService = showtimeService;
+    }
+
+    @PostMapping("v1/createShowtime")
+    ResponseEntity<String>createShowtime
 }
